156642abada365e7de70b72d7a8c9dde4b88c2f0
Almuz <almuz@inbox.lv>
2017-07-28 11:35:39 +0300

    1.2 PCR code changes

--- app/header.php
+++ app/header.php
@@ -29,6 +29,9 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>ul {
+            list-style-type: none;
+        }</style>
 </head>
 <body>
 
@@ -55,4 +58,4 @@
 
     <div class="row">
 
-        <div class="col-sm-8 blog-main">
+        <div class="col-sm-8 blog-main" style="text-align: center">
